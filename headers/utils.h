46238cf602ff03ef0a7a86d160557ca7b098398f
Lorenzo Affetti <affox90@gmail.com>
2014-02-17 12:59:32 +0100

    added utils to load image from file

---
+++ headers/utils.h
@@ -0,0 +1,70 @@
+#include "pgmp2.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+pgmp2_t load(char* filename){
+	FILE* in = NULL;
+	in = fopen(filename, "r");
+
+	if(in == NULL){
+		printf("Error in opening file\n");
+		exit(-1);
+	}
+	char* magic = (char*) malloc(sizeof(char)*2);
+	int width;
+	int height;
+	int max;
+	fscanf(in, "%s%*c", magic);
+	if(strcmp(magic, "P2")){
+		printf("Incorrect type of image, need a P2 image\n");
+		fclose(in);
+		exit(-1);
+	}
+
+	fscanf(in, "%d%*c", &width);
+	fscanf(in, "%d%*c", &height);
+	fscanf(in, "%d%*c", &max);
+
+	pgmp2_t img = init(width, height, max);
+	int pixel, i, j;
+	int min = 255;
+	for(i = 0; i < height; i++){
+		for(j = 0; j < width; j++){
+			if(feof(in)) break;
+			
+			fscanf(in, "%d%*c", &pixel);
+			set(img, i, j, pixel);
+			if(pixel < min){
+				min = pixel;
+			}
+		}
+	}
+
+	img.min = min;
+	fclose(in);
+
+	return img;
+}
+
+void store(pgmp2_t img){
+	FILE* out = NULL;
+	out = fopen("out.pgm", "w");
+
+	if(out == NULL){
+		printf("Error in creating file\n");
+		exit(-1);
+	}
+
+	fprintf(out, "P2\n");
+	fprintf(out, "%d %d\n", img.width, img.height);
+	fprintf(out, "%d\n", img.max);
+
+	int i, j;
+
+	for(i = 0; i < img.height; i++){
+		for(j = 0; j < img.width; j++){
+			fprintf(out, "%d ", get(img, i, j));
+		}
+		fprintf(out, "\n");
+	}
+}
--- normal.c
+++ normal.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "headers/utils.h"
 
 void usage(){
 	printf("norm <input-file> <new-min> <new-max>\n");
@@ -13,43 +12,15 @@ int normalize(int pixel, int min, int max, int new_min, int new_max){
 }
 
 int main(int argc, char** argv){
-	FILE* in = NULL;
-	FILE* out = NULL;
-
-	if(argc < 4){
+	/*if(argc < 4){
 		usage();
 		exit(-1);
 	}
 
 	int new_min = atoi(argv[2]);
-	int new_max = atoi(argv[3]);
+	int new_max = atoi(argv[3]);*/
 
-	in = fopen(argv[1], "r");
-	out = fopen("out.pgm", "w");
 
-	if(in == NULL || out == NULL){
-		printf("Error in opening file\n");
-		exit(-1);
-	}
-
-	char* magic = (char*) malloc(sizeof(char)*2);
-	int width;
-	int height;
-	int max;
-	fscanf(in, "%s%*c", magic);
-	fscanf(in, "%d%*c", &width);
-	fscanf(in, "%d%*c", &height);
-	fscanf(in, "%d%*c", &max);
-
-	fprintf(out, "%s %d %d %d\n", magic, width, height, new_max);
-
-	int pixel;
-	char separator;
-	while(!feof(in)){
-		fscanf(in, "%d%c", &pixel, &separator);
-		pixel = normalize(pixel, 0, max, new_min, new_max);
-		fprintf(out, "%d%c", pixel, separator);
-	}
-
-	return 0;
+	pgmp2_t img = load(argv[1]);
+	store(img);
 }
