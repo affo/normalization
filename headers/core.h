9c6a413d6ce30bc167d46fd1b9dfe7365b273613
Lorenzo Affetti <affox90@gmail.com>
2014-02-20 19:49:33 +0100

    Revert "error"
    This reverts commit def16e4cef93a8ecbec408236c6e54ccfd4484c4.

--- headers/core.h
+++ headers/core.h
@@ -17,15 +17,114 @@ typedef struct mpi_env {
 	int* receive_buffer;
 } mpi_env_t;
 
-mpi_env_t get_mpi_env(int, int, int, int);
+mpi_env_t get_mpi_env(int numtasks, int root, int rank, int length){
+	mpi_env_t* env = (mpi_env_t*) malloc(sizeof(mpi_env_t));
+	//if i'm the last one
+	env->numtasks = numtasks;
+	env->root = root;
+	env->rank = rank;
+	int chunk = length / numtasks;
+	if(rank == (numtasks - 1)){
+		chunk = length - (chunk * (numtasks - 1));
+	}
+	env->chunk = chunk;
+	//printf("rank: %d, chunk: %d\n", rank, chunk);
+		
+	env->receive_buffer = (int*) malloc(sizeof(int)*chunk);
+	
+
+	if(rank == 0){
+		env->offsets = (int*) malloc(sizeof(int)*numtasks);
+		env->buffer_sizes = (int*) malloc(sizeof(int)*numtasks);
+		for(int i = 0; i < numtasks; i++){
+		  	env->offsets[i] = i * chunk;
+		}
+		for(int i = 0; i < (numtasks - 1); i++){
+			env->buffer_sizes[i] = chunk;
+		}
+		env->buffer_sizes[numtasks - 1] = length - (chunk * (numtasks - 1));
+	}
+
+	return *env;
+}
 
 
-void normalize(pgmp2_t, int, int, mpi_env_t);
+void normalize(pgmp2_t img, int new_min, int new_max, mpi_env_t env){
+	int min = img.min;
+	int max = img.max;
+	double factor = (float)(new_max - new_min) / (float)(max - min);
 
-int get_max(pgmp2_t, mpi_env_t);
+	int* receive_buffer = env.receive_buffer;
+	int chunk = env.chunk;
 
-int get_min(pgmp2_t, mpi_env_t);
+	int i;
+	int chunk_size = chunk / omp_get_max_threads();
+	#pragma omp parallel for schedule(static, chunk_size) shared(receive_buffer) private(i) firstprivate(new_min, factor, min)
+		for(i = 0; i < chunk; i++){
+			receive_buffer[i] = (receive_buffer[i] - min) * factor + new_min;
+		}
 
-void scatter_pixels(pgmp2_t, mpi_env_t);
+	img.min = new_min;
+	img.max = new_max;
+}
 
-void gather_pixels(pgmp2_t, mpi_env_t);
+int get_max(pgmp2_t img, mpi_env_t env){
+	int* receive_buffer = env.receive_buffer;
+	int chunk = env.chunk;
+
+	int global_max = 0;
+	int local_max = 0;
+
+	#pragma omp parallel for
+		for (int i = 0; i < chunk; i++) {
+  			#pragma omp critical
+			 	if (receive_buffer[i] > local_max) local_max = receive_buffer[i];
+		}
+
+	MPI_Allreduce(&local_max, &global_max, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+
+	return global_max;
+}
+
+int get_min(pgmp2_t img, mpi_env_t env){
+	int* receive_buffer = env.receive_buffer;
+	int chunk = env.chunk;
+
+	int global_min = 255;
+	int local_min = 255;
+
+	#pragma omp parallel for
+		for (int i = 0; i < chunk; i++) {
+  			#pragma omp critical
+			 	if (receive_buffer[i] < local_min) local_min = receive_buffer[i];
+		}
+
+	MPI_Allreduce(&local_min, &global_min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+	
+
+	return global_min;
+}
+
+void scatter_pixels(pgmp2_t img, mpi_env_t env){
+	int* pixels = img.pixels;
+
+	int* buffer_sizes = env.buffer_sizes;
+	int* offsets = env.offsets;
+	int* receive_buffer = env.receive_buffer;
+	int chunk = env.chunk;
+	int root = env.root;
+
+	MPI_Scatterv(pixels, buffer_sizes, offsets, MPI_INT, receive_buffer, chunk, MPI_INT, root, MPI_COMM_WORLD);
+}
+
+void gather_pixels(pgmp2_t img, mpi_env_t env){
+	int* pixels = img.pixels;
+
+	int* buffer_sizes = env.buffer_sizes;
+	int* offsets = env.offsets;
+	int* receive_buffer = env.receive_buffer;
+	int chunk = env.chunk;
+	int root = env.root;
+
+	MPI_Gatherv(receive_buffer, chunk, MPI_INT, pixels, buffer_sizes, offsets, MPI_INT, root, MPI_COMM_WORLD);
+}
--- headers/pgmp2.h
+++ headers/pgmp2.h
@@ -8,8 +8,21 @@ typedef struct pgmp2 {
 	int* pixels;
 } pgmp2_t;
 
-pgmp2_t init(int, int);
+pgmp2_t init(int width, int height){
+	pgmp2_t* img = (pgmp2_t*) malloc(sizeof(pgmp2_t));
+	img->width = width;
+	img->height = height;
 
-int get(pgmp2_t, int);
+	//allocating memory for the matrix
+	img->pixels = (int*) malloc(sizeof(int)*width*height);
 
-void set(pgmp2_t, int, int);
+	return *img;
+}
+
+int get(pgmp2_t img, int i){
+	return img.pixels[i];
+}
+
+void set(pgmp2_t img, int i, int value){
+	img.pixels[i] = value;
+}
