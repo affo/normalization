7e10b00d37df093078b1b3c23d10a5f0726336f5
Lorenzo Affetti <affox90@gmail.com>
2014-02-20 18:36:06 +0100

    fixed an error in chunck calculation

--- headers/core.h
+++ headers/core.h
@@ -6,6 +6,7 @@
 #include <omp.h>
 #include <time.h>
 #include "mpi.h"
+
 typedef struct mpi_env {
 	int numtasks;
 	int rank;
@@ -23,10 +24,10 @@ mpi_env_t get_mpi_env(int numtasks, int root, int rank, int length){
 	env->root = root;
 	env->rank = rank;
 	int chunk = length / numtasks;
-	env->chunk = chunk;
 	if(rank == (numtasks - 1)){
 		chunk = length - (chunk * (numtasks - 1));
 	}
+	env->chunk = chunk;
 	printf("rank: %d, chunk: %d\n", rank, chunk);
 		
 	env->receive_buffer = (int*) malloc(sizeof(int)*chunk);
@@ -48,56 +49,34 @@ mpi_env_t get_mpi_env(int numtasks, int root, int rank, int length){
 }
 
 
-void normalize(pgmp2_t* img, int new_min, int new_max, mpi_env_t env){
-	int i;
-	int length = img->width * img->height;
-	int min = img->min;
-	int max = img->max;
-	int* pixels = img->pixels;
+void normalize(pgmp2_t img, int new_min, int new_max, mpi_env_t env){
+	int min = img.min;
+	int max = img.max;
 	double factor = (float)(new_max - new_min) / (float)(max - min);
 
-	int* buffer_sizes = env.buffer_sizes;
-	int* offsets = env.offsets;
 	int* receive_buffer = env.receive_buffer;
 	int chunk = env.chunk;
-	int root = env.root;
-
-	clock_t t = clock();
-	MPI_Scatterv(pixels, buffer_sizes, offsets, MPI_INT, receive_buffer, chunk, MPI_INT, root, MPI_COMM_WORLD);
 
 	//#pragma omp parallel for shared(recvbuf) private(i) firstprivate(new_min, factor, min)
-		for(i = 0; i < chunk; i++){
+		for(int i = 0; i < chunk; i++){
 			receive_buffer[i] = (receive_buffer[i] - min) * factor + new_min;
 		}
 
-	MPI_Gatherv(receive_buffer, chunk, MPI_INT, pixels, buffer_sizes, offsets, MPI_INT, root, MPI_COMM_WORLD);
-
-	t = clock() - t;
-	printf("Normalization: %fs\n", ((float)t) / CLOCKS_PER_SEC);
-
-	img->min = new_min;
-	img->max = new_max;
+	img.min = new_min;
+	img.max = new_max;
 }
 
 int get_max(pgmp2_t img, mpi_env_t env){
-	int* pixels = img.pixels;
-	int length = img.width * img.height;
-
-	int* buffer_sizes = env.buffer_sizes;
-	int* offsets = env.offsets;
 	int* receive_buffer = env.receive_buffer;
 	int chunk = env.chunk;
-	int root = env.root;
 
 	int global_max = 0;
 	int local_max = 0;
 
-	MPI_Scatterv(pixels, buffer_sizes, offsets, MPI_INT, receive_buffer, chunk, MPI_INT, root, MPI_COMM_WORLD);
-
 	#pragma omp parallel for
-		for (int i = 0; i < length; i++) {
+		for (int i = 0; i < chunk; i++) {
   			#pragma omp critical
-			 	if (pixels[i] > local_max) local_max = pixels[i];
+			 	if (receive_buffer[i] > local_max) local_max = receive_buffer[i];
 		}
 
 	MPI_Allreduce(&local_max, &global_max, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
@@ -106,8 +85,26 @@ int get_max(pgmp2_t img, mpi_env_t env){
 }
 
 int get_min(pgmp2_t img, mpi_env_t env){
+	int* receive_buffer = env.receive_buffer;
+	int chunk = env.chunk;
+
+	int global_min = 255;
+	int local_min = 255;
+
+	#pragma omp parallel for
+		for (int i = 0; i < chunk; i++) {
+  			#pragma omp critical
+			 	if (receive_buffer[i] < local_min) local_min = receive_buffer[i];
+		}
+
+	MPI_Allreduce(&local_min, &global_min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+	
+
+	return global_min;
+}
+
+void scatter_pixels(pgmp2_t img, mpi_env_t env){
 	int* pixels = img.pixels;
-	int length = img.width * img.height;
 
 	int* buffer_sizes = env.buffer_sizes;
 	int* offsets = env.offsets;
@@ -115,19 +112,17 @@ int get_min(pgmp2_t img, mpi_env_t env){
 	int chunk = env.chunk;
 	int root = env.root;
 
-	int global_min = 255;
-	int local_min = 255;
-
 	MPI_Scatterv(pixels, buffer_sizes, offsets, MPI_INT, receive_buffer, chunk, MPI_INT, root, MPI_COMM_WORLD);
+}
 
-	#pragma omp parallel for
-		for (int i = 0; i < length; i++) {
-  			#pragma omp critical
-			 	if (pixels[i] < local_min) local_min = pixels[i];
-		}
+void gather_pixels(pgmp2_t img, mpi_env_t env){
+	int* pixels = img.pixels;
 
-	MPI_Allreduce(&local_min, &global_min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
-	
+	int* buffer_sizes = env.buffer_sizes;
+	int* offsets = env.offsets;
+	int* receive_buffer = env.receive_buffer;
+	int chunk = env.chunk;
+	int root = env.root;
 
-	return global_min;
+	MPI_Gatherv(receive_buffer, chunk, MPI_INT, pixels, buffer_sizes, offsets, MPI_INT, root, MPI_COMM_WORLD);
 }
--- normal.c
+++ normal.c
@@ -6,8 +6,17 @@
 	#include "mpi.h"
 #endif
 
+#include <time.h>
+
 void usage(){
 	printf("norm <input-file> <new-min> <new-max>\n");
+}
+
+void print_time(char* str, clock_t t, int rank){
+	if(rank == 0){
+		t = clock() - t;
+		printf("%s: %fs\n", str, ((float)t) / CLOCKS_PER_SEC);
+	}
 }
 
 int main(int argc, char** argv){
@@ -15,25 +24,47 @@ int main(int argc, char** argv){
 		usage();
 		exit(-1);
 	}
-
-	int new_min = atoi(argv[2]);
-	int new_max = atoi(argv[3]);
-
-	pgmp2_t img = load(argv[1]);
 	
 	int numtasks, rank;
+	clock_t t;
+	pgmp2_t img;
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-	mpi_env_t env = get_mpi_env(numtasks, 0, rank, img.width * img.height);
+	
+	int length;
+	if(rank == 0){
+		t = clock();
+		img = load(argv[1]);
+		print_time("Image Loading", t, rank);
+		length = img.width * img.height;
+	}
+	int new_min = atoi(argv[2]);
+	int new_max = atoi(argv[3]);
 
+	MPI_Bcast(&length, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	mpi_env_t env = get_mpi_env(numtasks, 0, rank, length);
+
+	scatter_pixels(img, env);
+
+	t = clock();
 	img.max = get_max(img, env);
+	print_time("Getting MAX", t, rank);
+
+	t = clock();
 	img.min = get_min(img, env);
-	printf("max %d, min %d\n", img.max, img.min);
-	normalize(&img, new_min, new_max, env);
+	print_time("Getting MIN", t, rank);
+
+	t = clock();
+	normalize(img, new_min, new_max, env);
+	print_time("Normalizing", t, rank);
+
+	gather_pixels(img, env);
 
 	if(rank == 0){
+		t = clock();
 		store(img);
+		print_time("Storing", t, rank);
 	}
 	MPI_Finalize();
 }
