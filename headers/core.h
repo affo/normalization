b76f5cbb09d8129e296c4ab8c4464c3e59081d6b
Lorenzo Affetti <affox90@gmail.com>
2014-02-20 15:38:50 +0100

    first implementation of the core

--- headers/core.h
+++ headers/core.h
@@ -1,26 +1,53 @@
 #ifndef HEADER
-	#include "headers/pgmp2->h"
+	#include "headers/pgmp2.h"
+	#include <mpi.h>
 #endif
 
 #include <omp.h>
 #include <time.h>
-#include <mpi.h>
 
-void normalize(pgmp2_t* img, int new_min, int new_max){
-	double factor = (float)(new_max - new_min) / (float)(img->max - img->min);
-
+void normalize(pgmp2_t* img, int new_min, int new_max, int numtasks, int root){
 	int i;
 	int length = img->width * img->height;
 	int min = img->min;
+	int max = img->max
 	int* pixels = img->pixels;
-	int chunk = length / omp_get_max_threads();
+	double factor = (float)(new_max - new_min) / (float)(max - min);
 
 	clock_t t = clock();
 
-	#pragma omp parallel for schedule(static, chunk) shared(pixels) private(i) firstprivate(new_min, factor, min)
-		for(i = 0; i < length; i++){
+	int chunk = length / numtasks;
+	int *recvbuf, *sendcounts, *offsets;
+	//if you are the last one
+	if(rank == (numtasks - 1)){
+		chunk = length - (chunk * (numtasks - 1));
+	}
+	printf("rank: %d, chunk: %d\n", rank, chunk);
+
+	recvbuf = (int*) malloc(sizeof(int)*chunk);
+
+	if(rank == 0){
+		offsets = (int*) malloc(sizeof(int)*numtasks);
+		sendcounts = (int*) malloc(sizeof(int)*numtasks);
+		for(int i = 0; i < numtasks; i++){
+		  	offsets[i] = i * chunk;
+		}
+		for(int i = 0; i < (numtasks - 1); i++){
+		  sendcounts[i] = chunk;
+		}
+		sendcounts[numtasks - 1] = length - (chunk * (numtasks - 1));
+	}
+
+	sendcount = chunk;
+	recvcount = chunk;
+	MPI_Scatterv(pixels, sendcounts, offsets, MPI_INT, recvbuf, recvcount, MPI_INT, root, MPI_COMM_WORLD);
+
+	#pragma omp parallel for shared(recvbuf) private(i) firstprivate(new_min, factor, min)
+		for(i = 0; i < chunk; i++){
 			pixels[i] = (pixels[i] - min) * factor + new_min;
 		}
+
+	MPI_Gatherv(recvbuf, chunk, MPI_INT, pixels, sendcounts, offsets, MPI_INT, root, MPI_COMM_WORLD);
 
 	t = clock() - t;
 	printf("Normalization: %fs\n", ((float)t) / CLOCKS_PER_SEC);
@@ -29,14 +56,34 @@ void normalize(pgmp2_t* img, int new_min, int new_max){
 	img->max = new_max;
 }
 
-int get_max(int* array, int size){
-	int recv[size];
+int get_max(pgmp2_t img, int numtasks, int root){
+	int* pixels = img.pixels;
+	int length = img.width * img.height;
+	int recv[numtasks];
+	MPI_Reduce(pixels, recv, length, MPI_INT, MPI_MAX, root, MPI_COMM_WORLD);
 
-	MPI_Reduce(array, recv, MPI_INT, MPI_MAX, root, MPI_COMM_WORLD);
+	int max = 0;
+	for(int i = 0; i < numtasks; i++){
+		if(recv[i] > max){
+			max = recv[i];
+		}
+	}
 
-
+	return max;
 }
 
-int get_min(int* array, int size){
+int get_min(pgmp2_t img, int numtasks, int root){
+	int* pixels = img.pixels;
+	int length = img.width * img.height;
+	int recv[numtasks];
+	MPI_Reduce(pixels, recv, length, MPI_INT, MPI_MIN, root, MPI_COMM_WORLD);
 
+	int min = 255;
+	for(int i = 0; i < numtasks; i++){
+		if(recv[i] < max){
+			min = recv[i];
+		}
+	}
+
+	return min;
 }
--- normal.c
+++ normal.c
@@ -2,7 +2,8 @@
 #define HEADER
 	#include "headers/pgmp2.h"
 	#include "headers/utils.h"
-	#include "headers/normalize.h"
+	#include "headers/core.h"
+	#include <mpi.h>
 #endif
 
 void usage(){
@@ -19,6 +20,19 @@ int main(int argc, char** argv){
 	int new_max = atoi(argv[3]);
 
 	pgmp2_t img = load(argv[1]);
-	normalize(&img, new_min, new_max);
+	int length = img.width * img.height;
+	int* pixels = img.pixels;
+
+	int numtasks, rank, root;
+	MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+	int max = get_max(img, numtasks, root);
+	int min = get_min(img, numtasks, root);
+
+	normalize(&img, new_min, new_max, numtasks, root);
+
+	MPI_Finalize();
 	store(img);
 }
